2010a71648dd2c4206b52cc47cb5f9aa05f0241a
ericchow8@gmail.com <ericchow8@gmail.com>
2023-06-05 17:02:55 +0200

    initalisation

---
+++ src/Controller/TestController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * *Une méthode lié à une route DOIT retourner un objet de la classe Response.
+ * *Plusieurs méthodes de la classe AbstractController retourn un objet Response
+ * *(render, json..........)
+ */
+class TestController extends AbstractController
+{
+    #[Route('/test', name: 'app_test')]
+    public function index(): Response
+    {
+        /**
+         * *La méthode render va générer l'affichage qui va être envoyé au client
+         * *le 1er argument  (obligatoire) est le nom du fichier utilisé pour générer l'affichage.
+         * * Quand on utilise TWIG, le nom du fichier est toujours donné à partir du dossier 'templates'
+         * 
+         * *le 2ème argument (optionel) est de type array. il contient les variables que l'on veut passer à la vue
+         */
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'TestController',
+        ]);
+    }
+
+    #[Route('/test/salut')]
+    public function salut()
+    {
+      $prenom = "Eric";  
+      return $this->render('test/salut.html.twig',[
+        'prenom' => $prenom,
+      ]);
+    }
+
+    // *EXERCICE:
+/**
+ * *Ajouter une nouvelle route le chemin "test/calcul qui affiche le résultat du calcul 5+12
+ * 
+ */
+
+    #[Route('/test/calcul', name: 'app_calcul')]
+    public function calcul()
+    {
+        // $result = 5 + 12;
+        // return $this->render('test/calcul.html.twig',[
+        //    'result' => $result
+        // ]);
+
+        $a = 5;
+        $b = 12;
+        return $this->render('test/calcul.html.twig',[
+            'nb1' => $a,
+            'nb2' => $b,
+          ]);
+    }
+
+     /**
+     * *On peu ajouter des paramètres dans le chemin d'une route. Il faut les placer entre {}
+     * *Cela signifie que la partie de l'URL qui est entre {} sera remplacé par n'importe qu'elle chaine de caractères.
+     ** Pour récupérer les paramètres de l'URLil faut les passer dans les arguments de la méthode
+
+    **EXPRESSION irreguliers (REGEX)
+    **[0-9] : le caractère doit être compris entre les caractères 0 et 9
+    ** \d : le caractère doit être un chiffre (digit)
+    ** + : le caractère précédent peut-être présent 1 ou plusieurs fois
+    ** * : le caractère précédent peut-être  présent 0 ou plusieurs fois
+    ** ? : le caracètre précédent peut-être 0 ou 1 fois     
+     */
+
+    #[Route('/test/calcul/{a}/{b}', name:'app_test_calcul', requirements:["a" => "[0-9)]+", "b" => "\d+"])]
+    public function calculDynamique($a, $b)
+    {
+        return $this ->render('test/calcul.html.twig', [
+            "nb1" => $a,
+            "nb2" => $b,
+        ]);
+    }
+
+    // *EXERCICE
+    // * ajouter une route pour l'url "/test/salutation" suivi du prénom. La page affichera  "bonjour prenom" en remplacant prenom par le mot qui suit salutation dans l'url(donc il faut récupéerer le prénom à afficher dans l'url)
+
+    #[Route('/test/salutation/{p?}', name:'app-prenom')]
+    public function afficherPrenom($p)
+    {
+        if(!$p){
+            $p = "par défaut";
+        }
+        return $this->render('test/salutation.html.twig', [
+            "prenom" => $p
+        ]);
+    }
+
+}
+
+
